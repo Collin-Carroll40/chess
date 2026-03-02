aaa61ebc33f1ddef702fa1d0c225d282743cae13
Collin-Carroll40 <colindrewcarroll@gmail.com>
2026-03-01 18:40:17 -0700

    filled out first half of dao

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    AuthData createAuth(String username);
+    AuthData getAuth(String authToken);
+    void deleteAuth(String authToken);
+    void clear();
 }
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private final HashMap<String, AuthData> auths = new HashMap<>();
+
+    @Override
+    public AuthData createAuth(String username) {
+        // Generate a random secure token
+        String authToken = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(authToken, username);
+        auths.put(authToken, auth);
+        return auth;
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) {
+        auths.remove(authToken);
+    }
+
+    @Override
+    public void clear() {
+        auths.clear();
+    }
 }
