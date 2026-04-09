2ef0d90c2509cc1a6df2a1cd0c695ff0265bc8d8
Collin-Carroll40 <colindrewcarroll@gmail.com>
2026-04-09 01:15:44 -0600

    changed boardDrawer.java

--- client/src/main/java/client/BoardDrawer.java
+++ client/src/main/java/client/BoardDrawer.java
@@ -2,6 +2,10 @@ package client;
 
 import chess.*;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import static client.EscapeSequences.*;
 
 public class BoardDrawer {
@@ -9,6 +13,20 @@ public class BoardDrawer {
     private static final String[] COL_LABELS = {"a", "b", "c", "d", "e", "f", "g", "h"};
 
     public static void drawBoard(ChessBoard board, boolean whitePerspective) {
+        drawBoardInternal(board, whitePerspective, null, null);
+    }
+
+    public static void drawBoardWithHighlights(ChessBoard board, boolean whitePerspective,
+                                               ChessPosition selectedPos, Collection<ChessMove> moves) {
+        Set<ChessPosition> highlightSquares = new HashSet<>();
+        for (ChessMove move : moves) {
+            highlightSquares.add(move.getEndPosition());
+        }
+        drawBoardInternal(board, whitePerspective, selectedPos, highlightSquares);
+    }
+
+    private static void drawBoardInternal(ChessBoard board, boolean whitePerspective,
+                                          ChessPosition selectedPos, Set<ChessPosition> highlights) {
         System.out.println();
 
         int rowStart = whitePerspective ? 8 : 1;
@@ -19,7 +37,6 @@ public class BoardDrawer {
         int colEnd = whitePerspective ? 8 : 1;
         int colStep = whitePerspective ? 1 : -1;
 
-        // Column headers
         System.out.print("   ");
         for (int c = colStart; c != colEnd + colStep; c += colStep) {
             System.out.print(" " + COL_LABELS[c - 1] + " ");
@@ -29,10 +46,19 @@ public class BoardDrawer {
         for (int r = rowStart; r != rowEnd + rowStep; r += rowStep) {
             System.out.print(" " + r + " ");
             for (int c = colStart; c != colEnd + colStep; c += colStep) {
+                ChessPosition pos = new ChessPosition(r, c);
                 boolean lightSquare = (r + c) % 2 == 1;
-                String bg = lightSquare ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREEN;
 
-                ChessPiece piece = board.getPiece(new ChessPosition(r, c));
+                String bg;
+                if (selectedPos != null && selectedPos.equals(pos)) {
+                    bg = SET_BG_COLOR_YELLOW;
+                } else if (highlights != null && highlights.contains(pos)) {
+                    bg = lightSquare ? SET_BG_COLOR_GREEN : SET_BG_COLOR_DARK_GREEN;
+                } else {
+                    bg = lightSquare ? SET_BG_COLOR_LIGHT_GREY : SET_BG_COLOR_DARK_GREEN;
+                }
+
+                ChessPiece piece = board.getPiece(pos);
                 String pieceStr = getPieceString(piece);
 
                 System.out.print(bg + pieceStr + RESET_BG_COLOR);
@@ -40,7 +66,6 @@ public class BoardDrawer {
             System.out.println(" " + r);
         }
 
-        // Column footers
         System.out.print("   ");
         for (int c = colStart; c != colEnd + colStep; c += colStep) {
             System.out.print(" " + COL_LABELS[c - 1] + " ");
